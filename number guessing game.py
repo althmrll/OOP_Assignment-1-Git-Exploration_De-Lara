f0cb848011c503de900c7da06cbaac0e8900ec24
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-02-25 15:46:30 +0800

    game mechanics

--- number guessing game.py
+++ number guessing game.py
@@ -1,4 +1,10 @@
+import random
+score = 0
+
 #Listing game mechanics
+def game_mechanics():
+    print ("NUMBER GUESSING GAME\n")
+    print ("\nThis is a number guessing game. The game wil choose a number ranging from 1 to 100 and you will have to guess what number the game chose. Don't worry the game will tell you if you guess higher or lower. Good luck!")
 #Pressing enter to start
 #Game itself
 #Scoring
